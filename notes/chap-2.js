7c65a91572993dbaf6ce8ff6018925bc347cb3e5
nasghoi <nasrullahjamsarim@gmail.com>
2025-05-16 18:36:53 +0800

    learn chapter 2

---
+++ notes/chap-2.js
@@ -0,0 +1,15 @@
+// look the javascript for beginner repo for most of this chapter notes
+const repoLink = "https://github.com/nasghoi/javascript-for-beginner"
+console.log(repoLink)
+
+true && false // this will return false
+
+let quotes = {
+    nas: 'is cool',
+    nab: 'isn\'t cool, she\'s hot', // use backslash before the single quote to escape it
+    number_of_our_child: 3,
+    is_we_happily_married: true,
+}
+
+console.log(quotes.nab)
+console.log(console) // this will print the console object
